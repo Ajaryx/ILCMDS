64bd39255e741ab9c3aafe97350133f83478c00d
Ajaryx <impressum15@gmail.com>
2025-07-03 15:47:06 +0200

    finished AppStatusMenuLayoutBuilder options

--- src/menus/AppStatusMenu.cpp
+++ src/menus/AppStatusMenu.cpp
@@ -45,23 +45,106 @@ AppStatusMenuLayoutBuilder AppStatusMenuLayoutBuilder::Warning(const std::string
 	const std::string& description,
 	std::function<void()> on_click)
 {
-	
 
-	return AppStatusMenuLayoutBuilder();
+	Component btn = Button("OK", on_click);
+
+
+	AppStatusMenuLayoutBuilder builder;
+
+
+	builder.headline = headline;
+	builder.description = description;
+
+
+	builder.layout = Renderer(btn, [=]()
+		{
+			return vbox({
+
+				text(builder.headline) | underlined | bold | center,
+				filler() | size(HEIGHT, EQUAL, 1),
+				text("WARNING") | underlined | color(Color::Orange1) | center,
+				filler() | size(HEIGHT, EQUAL, 2),
+
+				paragraphAlignCenter(builder.description) | borderHeavy | color(Color::Orange1) | hcenter,
+				filler() | size(HEIGHT, EQUAL, 2),
+				btn->Render(),
+
+				});
+
+		});
+
+	return builder;
 }
 AppStatusMenuLayoutBuilder AppStatusMenuLayoutBuilder::FatalError(const std::string& headline,
 	const std::string& description,
 	const std::function<void()> on_click)
 {
+	ButtonOption opt = ButtonOption::Border();
 
-	return AppStatusMenuLayoutBuilder();
+	Component btn = Button("OK", on_click);
+
+
+	AppStatusMenuLayoutBuilder builder;
+
+
+	builder.headline = headline;
+	builder.description = description;
+
+
+	builder.layout = Renderer(btn, [=]()
+		{
+			return vbox({
+
+				text(builder.headline) | underlined | bold | center,
+				filler() | size(HEIGHT, EQUAL, 1),
+				text("FATAL ERROR") | underlined | color(Color::Red1) | center,
+				filler() | size(HEIGHT, EQUAL, 2),
+
+				paragraphAlignCenter(builder.description) | borderHeavy | color(Color::Red1) | hcenter,
+				filler() | size(HEIGHT, EQUAL, 2),
+				btn->Render(),
+
+				});
+
+		});
+
+	return builder;
 }
 AppStatusMenuLayoutBuilder AppStatusMenuLayoutBuilder::Choose(const std::string& headline,
 	const std::string& description,
 	std::function<void()> on_click_Yes,
 	std::function<void()> on_click_No)
 {
-	return AppStatusMenuLayoutBuilder();
+
+	Component btnYes = Button("OK", on_click_Yes);
+	Component btnNo = Button("NO", on_click_No);
+
+	AppStatusMenuLayoutBuilder builder;
+
+	builder.headline = headline;
+	builder.description = description;
+
+	Component btnCont = Container::Vertical({ btnYes, btnNo });
+
+	builder.layout = Renderer(btnCont, [=]()
+		{
+			return vbox({
+
+				text(builder.headline) | underlined | bold | center,
+				filler() | size(HEIGHT, EQUAL, 1),
+				text("CHOOSE AN OPTION") | underlined | center,
+				filler() | size(HEIGHT, EQUAL, 2),
+
+				paragraphAlignCenter(builder.description) | borderHeavy | hcenter,
+				filler() | size(HEIGHT, EQUAL, 2),
+				btnYes->Render(),
+				btnNo->Render(),
+
+				});
+
+		});
+
+	return builder;
 }
 /*-------------------------------------------*/
 
